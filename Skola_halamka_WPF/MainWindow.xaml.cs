34ee0053d41abc2466744843a53d52b6963d69ce
Lubos <lubos.halamka@seznam.cz>
2024-10-05 17:10:08 +0200

    Přidejte soubory projektu.

---
+++ Skola_halamka_WPF/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace Skola_halamka_WPF
+{
+    public partial class MainWindow : Window
+    {
+        
+        ObservableCollection<string> Teachers = new ObservableCollection<string>();
+        ObservableCollection<string> Maintenance = new ObservableCollection<string>();
+        ObservableCollection<string> Support = new ObservableCollection<string>();
+        ObservableCollection<string> Students = new ObservableCollection<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+           
+            TeachersList.ItemsSource = Teachers;
+            MaintenanceList.ItemsSource = Maintenance;
+            SupportList.ItemsSource = Support;
+            StudentsList.ItemsSource = Students;
+        }
+
+       
+        private void AddTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            var addMemberWindow = new Window1();
+            if (addMemberWindow.ShowDialog() == true) 
+            {
+                Teachers.Add(addMemberWindow.FullName); 
+            }
+        }
+
+        private void DeleteTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            if (TeachersList.SelectedItem != null)
+                Teachers.Remove((string)TeachersList.SelectedItem);
+        }
+
+        private void AddMaintenance_Click(object sender, RoutedEventArgs e)
+        {
+            var addMemberWindow = new Window1();
+            if (addMemberWindow.ShowDialog() == true)
+            {
+                Maintenance.Add(addMemberWindow.FullName);
+            }
+        }
+
+        private void DeleteMaintenance_Click(object sender, RoutedEventArgs e)
+        {
+            if (MaintenanceList.SelectedItem != null)
+                Maintenance.Remove((string)MaintenanceList.SelectedItem);
+        }
+
+        private void AddSupport_Click(object sender, RoutedEventArgs e)
+        {
+            var addMemberWindow = new Window1();
+            if (addMemberWindow.ShowDialog() == true)
+            {
+                Support.Add(addMemberWindow.FullName);
+            }
+        }
+
+        private void DeleteSupport_Click(object sender, RoutedEventArgs e)
+        {
+            if (SupportList.SelectedItem != null)
+                Support.Remove((string)SupportList.SelectedItem);
+        }
+
+        private void AddStudent_Click(object sender, RoutedEventArgs e)
+        {
+            var addMemberWindow = new Window1();
+            if (addMemberWindow.ShowDialog() == true)
+            {
+                Students.Add(addMemberWindow.FullName);
+            }
+        }
+
+        private void DeleteStudent_Click(object sender, RoutedEventArgs e)
+        {
+            if (StudentsList.SelectedItem != null)
+                Students.Remove((string)StudentsList.SelectedItem);
+        }
+    }
+}
---
+++ Skola_halamka_WPF/Window1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Skola_halamka_WPF
+{
+   
+    public partial class Window1 : Window
+    {
+        public string FullName { get; private set; }
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            string firstName = FirstNameTextBox.Text.Trim();
+            string lastName = LastNameTextBox.Text.Trim();
+
+            
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            {
+               
+                MessageBox.Show("Vyplň celé jméno", "Chyba", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return; 
+            }
+
+            
+            FullName = $"{firstName} {lastName}";
+            DialogResult = true;
+            Close();
+        }
+
+    }
+}
